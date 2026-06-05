94aad860ff7236708f2beac424bb11c9399ff8c0
Jimmie Fulton <jimmie@ybor.ai>
2026-06-05 12:40:53 -0700

    feat: java-graphql-service-archetype archetype content (initial import for review)

---
+++ contents/base/{{ project-name }}/{{ project-name }}-graphql/src/main/java/{{ root_directory }}/graphql/HealthDataFetcher.java
@@ -0,0 +1,13 @@
+package {{ group_id }}.graphql;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HealthDataFetcher {
+
+    @QueryMapping
+    public String health() {
+        return "OK";
+    }
+}
